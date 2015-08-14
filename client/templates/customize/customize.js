1b07d2221a1de99630c551f433a0cbc22e9f9e5f
James R. Zygmont <j.r.zygmont@gmail.com>
2015-08-14 14:48:05 -0400

    Load champions on customize page

---
+++ client/templates/customize/customize.js
@@ -0,0 +1,11 @@
+if (Meteor.isClient) {
+    Template.customize.rendered = function () {
+
+    }
+
+    Template.customize.helpers({
+        'champions' : function() {
+            return champions.find({}, {sort: {name: 1}});
+        },
+    });
+}
