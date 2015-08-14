28566e723af03d138fb7d3718a705813141d20ae
Junjie (Ryan) Zhang <jzhang8299@gmail.com>
2015-08-14 04:20:09 -0400

    added navbar,footer,loading,home template

--- client/lib/router.js
+++ client/lib/router.js
@@ -1,3 +1,19 @@
-﻿Router.route('/', {
-    name: 'items'
+﻿
+
+Router.configure({
+	'layoutTemplate': "layout",
+	 loadingTemplate: 'loading'
+
+});
+Router.route('/items', {
+	name: 'items'
+
+});
+Router.route('/customize', {
+	name: 'customize'
+	
+});
+Router.route('/', {
+	name: 'home'
+	
 });
--- server/server.js
+++ server/server.js
@@ -1,2 +1,7 @@
 ﻿items = new Mongo.Collection("items");
 champions = new Mongo.Collection("champions");
+
+Houston.add_collection(Meteor.users);
+Houston.add_collection(Houston._admins);
+Houston.add_collection(items);
+Houston.add_collection(champions);
