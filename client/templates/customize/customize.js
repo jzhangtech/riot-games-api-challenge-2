f260cfac6d1eab4cf1710b8cbce1cc72d4ae27b1
James R. Zygmont <j.r.zygmont@gmail.com>
2015-08-16 14:46:45 -0400

    Load items by category and display their images

--- client/templates/customize/customize.js
+++ client/templates/customize/customize.js
@@ -1,11 +1,30 @@
 if (Meteor.isClient) {
+    var tags = ["Health", "Spell Damage", "Armor", "Damage", "Life Steal", "Attack Speed", "Critical Strike", "Movement", "Active", "Trinket", "Spell Block", "Health Regen", "Jungle", "Mana Regen", "On Hit", "Cooldown Reduction", "Aura", "Armor Penetration", "Boots", "Stealth", "Vision", "Magic Penetration", "Mana", "Consumable", "Lane", "GoldPer", "Slow", "Tenacity", "SpellVamp"];
+    
+    Session.set('selectedTag', 'empty');
+    
     Template.customize.rendered = function () {
 
     }
 
     Template.customize.helpers({
-        'champions' : function() {
-            return champions.find({}, {sort: {name: 1}});
+        'champions': function () {
+            return champions.find({}, { sort: { name: 1 } });
         },
+
+        'itemTypes': function () {
+            return tags;
+        },
+        
+        'listItems': function () {
+            var selectedTag = Session.get('selectedTag');
+            return items.find({tags: selectedTag});
+        }
+    });
+    
+    Template.customize.events({
+        'change #itemTypes': function (e, t) {
+            Session.set('selectedTag', t.find('#itemTypes').value.replace(/\s+/g, ''));
+        }
     });
 }
